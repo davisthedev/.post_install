f0cf014b1cb50107cf433f4bce8b2108e14d3939
Davis <davis@pop-os.lan>
2024-11-04 16:14:31 -0500

    remove mise ruby

--- popos/terminal/post-mise.sh
+++ popos/terminal/post-mise.sh
@@ -2,6 +2,4 @@ mise use --global node@lts
 mise use --global go@latest
 mise use --global python@latest
 mise use --global java@latest
-mise use --global ruby@3.3
-mise x ruby -- gem install rails --no-document
 bash -c "$(curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs)" -- -y
