c0da3a03517d163008319781f87165e1e1b05392
Davis Sanders <davisthedev@protonmail.com>
2024-11-05 15:07:46 -0500

    fix y/n prompt

--- popos/deps/01_install_base_packages.sh
+++ popos/deps/01_install_base_packages.sh
@@ -1,10 +1,10 @@
 sudo apt update
 sudo apt -y upgrade
-sudo apt dist-upgrade
-sudo apt autoremove
+sudo apt -y dist-upgrade
+sudo apt -y autoremove
 sudo apt autoclean
 sudo fwupdmgr get-devices
 sudo fwupdmgr get-updates
 sudo fwupdmgr update
-sudo apt install curl git unzip stow
+sudo apt -y install curl git unzip stow
 
